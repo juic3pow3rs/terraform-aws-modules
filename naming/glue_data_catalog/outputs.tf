439502e27303d02b5ac75921ddfa14b45d612eee
juic3pow3rs <juic3pow3rs@users.noreply.github.com>
2022-11-17 12:10:35 +0100

    fix output variable names

--- naming/glue_data_catalog/outputs.tf
+++ naming/glue_data_catalog/outputs.tf
@@ -1,7 +1,7 @@
-output "module.naming.catalog_db" {
+output "catalog_db" {
   value = "${var.prefix}-${var.team_shortname}-${var.resource_shortname}-${var.role}-${var.environment}-db"
 }
 
-output "module.naming.catalog_crawler" {
+output "catalog_crawler" {
   value = "${var.prefix}-${var.team_shortname}-${var.resource_shortname}-${var.role}-${var.environment}-crawler"
 }
