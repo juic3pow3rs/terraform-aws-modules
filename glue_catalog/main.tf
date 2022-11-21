01b438666488cdd42d6a75e12a200dc9d0f96c2d
juic3pow3rs <juic3pow3rs@users.noreply.github.com>
2022-11-21 11:59:25 +0100

    replace default permission with default permission extended with wider s3 access

--- glue_catalog/main.tf
+++ glue_catalog/main.tf
@@ -8,7 +8,7 @@ module "naming" {
 }
 
 resource "aws_iam_role" "glue" {
-  name = "AWSGlueServiceRoleDefault"
+  name = "AWSGlueServiceRoleUni"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -26,9 +26,113 @@ resource "aws_iam_role" "glue" {
 EOF
 }
 
+resource "aws_iam_policy" "glue_crawler_policy" {
+  name        = "AWSGlueCrawlerPolicy"
+  path        = "/"
+  description = "Policy for AWS Glue Crawler"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "glue:*",
+                "s3:GetBucketLocation",
+                "s3:ListBucket",
+                "s3:ListAllMyBuckets",
+                "s3:GetBucketAcl",
+                "ec2:DescribeVpcEndpoints",
+                "ec2:DescribeRouteTables",
+                "ec2:CreateNetworkInterface",
+                "ec2:DeleteNetworkInterface",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeVpcAttribute",
+                "iam:ListRolePolicies",
+                "iam:GetRole",
+                "iam:GetRolePolicy",
+                "cloudwatch:PutMetricData"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:CreateBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*/*",
+                "arn:aws:s3:::*/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:logs:*:*:/aws-glue/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateTags",
+                "ec2:DeleteTags"
+            ],
+            "Condition": {
+                "ForAllValues:StringEquals": {
+                    "aws:TagKeys": [
+                        "aws-glue-service-resource"
+                    ]
+                }
+            },
+            "Resource": [
+                "arn:aws:ec2:*:*:network-interface/*",
+                "arn:aws:ec2:*:*:security-group/*",
+                "arn:aws:ec2:*:*:instance/*"
+            ]
+        }
+    ]
+},
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "glue_service" {
     role = aws_iam_role.glue.id
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+    policy_arn = aws_iam_policy.glue_crawler_policy.arn
 }
 
 resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
